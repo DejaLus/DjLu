ce10004ff34f334cb3cc878edaf304ca32b12813
Thomas Robert <trobert94@gmail.com>
2016-03-25 23:42:10 +0100

    Add first version of papers list
    List papers in the user's git repo
    Design of the papers page
    Handles tags (read-only) with colors

---
+++ app/lib/formatting.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace lib;
+
+/**
+ * This is a utility class to do "advanced" (in the sense that it would be cumbersome to write only
+ * in template syntax) formating for various inputs.
+ */
+class Formatting {
+
+    public static $MONTHS = array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
+
+    /**
+     * Format an author's name.
+     *
+     * @param  string $author string to format (hopefully as "LastName, FirstName" or "FirstName LastName")
+     * @return string         formatted string
+     */
+    public static function formatAuthor($author, $firstName = true) {
+        $author = trim($author);
+
+        if (strpos($author, ",") !== false) {
+            $parts = explode(",", $author);
+            if (!$firstName)
+                return trim(array_shift($parts));
+            else
+                return trim(array_shift($parts))." ".implode(array_map(function($s){ return trim($s)[0]; }, $parts)).".";
+        }
+        elseif (strpos($author, " ") !== false) {
+            $parts = explode(" ", $author);
+            if (!$firstName)
+                return trim(array_pop($parts));
+            else
+                return trim(array_pop($parts))." ".implode(array_map(function($s){ return trim($s)[0]; }, $parts)).".";
+        }
+        else
+            return $author;
+    }
+
+    /**
+     * Format a list of authors.
+     *
+     * @param  string[] $authors array of strings of authors
+     * @param  string $format  format to use
+     * @return string          formated string
+     */
+    public static function formatAuthors($authors, $format = "short") {
+
+        // full format
+        if ($format == "full")
+            return implode(", ", array_map("self::formatAuthor", $authors));
+
+        // short format (X | X & Y | X et al.)
+        else {
+            if (count($authors) == 0)
+                return "";
+            elseif (count($authors) == 1)
+                return self::formatAuthor($authors[0], false);
+            elseif (count($authors) == 2)
+                return self::formatAuthor($authors[0], false)." & ".self::formatAuthor($authors[1], false);
+            else
+                return self::formatAuthor($authors[0], false)." <em>et al.</em>";
+        }
+    }
+
+    /**
+     * Format rating to display a list of stars
+     * @param  numeric  $rating
+     * @param  boolean $showEmpty show a span even if no rating is given
+     * @return string  formated string
+     */
+    public static function formatRating($rating, $showEmpty = false) {
+        if (is_numeric($rating)) {
+            $out = '<span class="rating" title="'.$rating.' / 5">';
+            for ($i = 0; $i < floor(min($rating, 5)); $i++)
+                $out .= '<i class="fa fa-star"></i>';
+            if ($rating - floor($rating) > 0.25) {
+                $out .= '<i class="fa fa-star-half-o"></i>';
+                $i++;
+            }
+            for (; $i < 5; $i++)
+                $out .= '<i class="fa fa-star-o"></i>';
+            return $out.'</span>';
+        }
+        elseif ($showEmpty)
+            return '<span class="rating rating-empty"></span>';
+
+    }
+
+    /**
+     * Format a date to be compact
+     * @param  string $date YYYY-MM-DD HH:MM date string
+     * @return string       DD monthName HHh string
+     */
+    public static function formatDate($date) {
+        return '<span class="date" title="'.$date.'">'.
+            preg_replace_callback('#^[0-9]+-([0-9]+)-([0-9]+)[^0-9]+([0-9]+)\:[0-9]+$#', function ($els) {
+                return $els[2].' '.self::$MONTHS[(int) $els[1]].' '.$els[3].'h';
+            }, $date).'</span>';
+    }
+
+    /**
+     * Format a list of tags, possibly using an array of colors
+     * @param  array $tags   list of tags
+     * @param  array  $tagsDB associative array (key = tag, value = hex color)
+     * @return string         formated string
+     */
+    public static function formatTags($tags, $tagsDB = array()) {
+
+        $out = "";
+
+        if (is_array($tags)) {
+            foreach ($tags as $tag) {
+                $bg = isset($tagsDB[$tag]) ? $tagsDB[$tag] : "#777";
+                $fg = isset($tagsDB[$tag]) ? self::textColorFromBgColor($tagsDB[$tag]) : "#fff";
+                $out .= '<span class="label label-default" style="background: '.$bg.'; color: '.$fg.'">'.$tag.'</span> ';
+            }
+        }
+
+        return $out;
+    }
+
+    /**
+     * Given an hex value of background color, which color should we use for
+     * the text?
+     *
+     * This computation is based on W3C recommandations [1], computation of
+     * luminance L is based on W3C [2] and conversion from sRGB to RGB based on
+     * ITU-R recommendation BT.709 [3].
+     *
+     * Overall process based on [4].
+     *
+     * Actual luminance threshold is supposed to be 0.179 based on W3C recommandations,
+     * but it was actually set to 0.33 based on trials and probably personnal taste.
+     *
+     * [1] http://www.w3.org/TR/WCAG20/
+     * [2] http://www.w3.org/TR/WCAG20/#relativeluminancedef
+     * [3] http://en.wikipedia.org/wiki/Luma_(video)#Rec._601_luma_versus_Rec._709_luma_coefficients
+     * [4] http://stackoverflow.com/a/3943023
+     *
+     * @param  string $hexBgColor background hex color
+     * @return string             foreground gex color
+     */
+    public static function textColorFromBgColor ($hexBgColor) {
+
+        // extract components
+        list($r, $g, $b) = sscanf($hexBgColor, "#%02x%02x%02x");
+
+        // fix sRGB to RGB
+        $r /= 255;
+        $g /= 255;
+        $b /= 255;
+        $r = ($r <= 0.03928) ? $r / 12.92 : pow(($r + 0.055) / 1.055, 2.4);
+        $g = ($g <= 0.03928) ? $g / 12.92 : pow(($g + 0.055) / 1.055, 2.4);
+        $b = ($b <= 0.03928) ? $b / 12.92 : pow(($b + 0.055) / 1.055, 2.4);
+
+        // compute luminance
+        $L = 0.2126 * $r + 0.7152 * $g + 0.0722 * $b;
+
+        // decide
+        return ($L > 0.33) ? "#000000" : "#ffffff";
+    }
+
+}
--- app/pages/papers.php
+++ app/pages/papers.php
@@ -9,10 +9,12 @@ class Papers {
 
     private $f3;
     private $user;
+    private $model;
 
     public function __construct () {
         $this->f3 = \Base::instance();
         $this->user = \models\User::instance();
+        $this->model = \models\Papers::instance();
     }
 
     /**
@@ -24,25 +26,16 @@ class Papers {
         if (!$this->user->isLoggedIn())
             $this->f3->reroute("@home");
 
-        // TODO list papers
+        // get papers list
+        $papers = $this->model->getPapers();
+        $addDates = array_map(function ($x) { return $x["date_added"]; }, $papers);
+        array_multisort($addDates, SORT_DESC, $papers);
 
-        // mockup list for preview
-        $papers = array(
-            array(
-            "author" => "Frome, Andrea and Corrado, Greg S. and Shlens, Jon and Bengio, Samy and Dean, Jeff and Ranzato, MarcAurelio and Mikolov, Tomas",
-            "title" => "DeViSE: A Deep Visual-Semantic Embedding Model",
-            "year" => "2013"),
-            array(
-            "author" => "Kawano, Yoshiyuki and Yanai, Keiji",
-            "title" => "Food image recognition with deep convolutional features",
-            "year" => "2014"),
-            array(
-            "author" => "Chatfield, Ken and Simonyan, Karen and Vedaldi, Andrea and Zisserman, Andrew",
-            "title" => "Return of the Devil in the Details: Delving Deep into Convolutional Nets",
-            "year" => "2014")
-        );
+        // gather tags
+        $tags = $this->model->getTags($papers);
 
         $this->f3->set("papers", $papers);
+        $this->f3->set("tags", $tags);
         $this->f3->set("content", "papers.htm");
         echo \Template::instance()->render("layout.htm");
     }
