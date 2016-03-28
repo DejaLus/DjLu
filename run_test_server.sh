951f17d7bbe7b94427fd1a17e0ae3b4a77c499a0
Thomas Robert <trobert94@gmail.com>
2016-03-28 03:27:21 +0200

    Rename config.dist.ini to config.ini
    Using a distribuable config file is not very useful in our case
    because there are no password or other sensitive info in the file
    and this will prevent future oversight to commit the updated conf.

--- run_test_server.sh
+++ run_test_server.sh
@@ -6,10 +6,5 @@ if [ $BASEDIR = '.' ]; then
 	BASEDIR=$(pwd)
 fi
 
-# Copy distributed config as config if no config done
-if [ ! -f config.ini ]; then
-	cp config.dist.ini config.ini
-fi
-
 # Run server
 php -S localhost:8080 -t $BASEDIR
